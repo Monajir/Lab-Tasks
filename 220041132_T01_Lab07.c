5b464d11d9c27cde0946906688dee063ef7279ec
Monajir <monazirmohammedminhaaz@gmail.com>
2024-04-29 22:03:31 +0600

    added prepend and append

--- 220041132_T01_Lab07.c
+++ 220041132_T01_Lab07.c
@@ -1,3 +1,5 @@
+// Implementation of Doubly Linked List
+
 #include <stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
@@ -5,11 +7,40 @@
 typedef struct node{
     int value;
     struct node *next;
+    struct node *prev;
 }node;
 
 node *list = NULL;
 
 void prepend(int element){
+    node *n = (node *)malloc(sizeof(node));
+
+    n->value = element;
+    n->prev = NULL;
+
+    list->prev = n;
+    n->next = list;
+    list = n;
+
+}
+
+void append(int element){
+    node *n = (node *)malloc(sizeof(node));
+    n -> value = element;
+    n -> next = NULL;
+    n -> prev = NULL;
+
+    node *temp = list;
+
+    while (temp -> next != NULL)
+    {
+         temp = temp -> next;
+    }  
+    n -> prev = temp;
+    temp -> next = n;   
+}
+
+void insert_sorted(int element){
     
 }
 
