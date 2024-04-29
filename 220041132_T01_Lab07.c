8ba658645ccc608aa596ddda0b79d8d8eaa90d93
Monajir <monazirmohammedminhaaz@gmail.com>
2024-04-29 20:25:19 +0600

    linked list for Structured Programming 2 Lab task

---
+++ 220041132_T01_Lab07.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef struct node{
+    int value;
+    struct node *next;
+}node;
+
+node *list = NULL;
+
+void prepend(int element){
+    
+}
+
+int main(){
+
+
+}
